3e9bcd912985810e561a3948a1116edae77b07f6
Htech00 <htechsolutionz@gmail.com>
2025-08-01 22:03:29 +0100

    first commit

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,15 +3,14 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, "..", "uploads");
-    console.log("Saving file to:", uploadPath);
-    cb(null, uploadPath);
+    // Must pass null as first argument for no error
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
+    // Unique name, including extension
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
-
 
 const upload = multer({ storage });
 
