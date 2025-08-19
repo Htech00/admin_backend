9d26bbebe3df67efd04fe3768a65804f42d5b777
Htech00 <htechsolutionz@gmail.com>
2025-08-19 16:32:49 +0100

    first commit

--- controller/adminController.js
+++ controller/adminController.js
@@ -68,10 +68,10 @@ const deleteAdmin = async (req, res) => {
 
     const deletedAdmin = await adminLogin.findByIdAndDelete(id);
     if (!deletedAdmin) {
-      return res.status(404).json({ message: "Property not found" });
+      return res.status(404).json({ message: "Admin not found" });
     }
 
-    res.status(200).json({ message: "Property deleted successfully" });
+    res.status(200).json({ message: "Admin deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
   }
