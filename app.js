89b359e1451464840f61ce19b498ff9c571b4ee1
Htech00 <htechsolutionz@gmail.com>
2025-09-17 13:19:15 +0100

    a

--- app.js
+++ app.js
@@ -44,19 +44,18 @@ app.use((req, res, next) => {
 const port = process.env.PORT || 5000;
 
 // ====== Middlewares ======
-app.use(cors())
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
